56e6083437acfe4f871348f7872d4702c780b8b2
mNiloy695 <mniloy695@gmail.com>
2025-09-09 21:07:55 +0600

    env added

--- Town_Market/settings.py
+++ Town_Market/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ="sendingemail695@gmail.com"
-EMAIL_HOST_PASSWORD ="bqza iind sufh cnef"
+EMAIL_HOST_USER =config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =config("EMAIL_HOST_PASSWORD")
 #silk settings
 # if DEBUG:
 #     INSTALLED_APPS += ['silk']
